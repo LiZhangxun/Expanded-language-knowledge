f2f21d2f38f94cc7565a248104786a48c8204016
LiZhangxun <2367621097@qq.com>
2021-10-13 20:26:08 +0800

    This project is about Python knowledge

--- Chapter_12-函数基础/07-函数嵌套调用应用-函数计算.py
+++ Chapter_12-函数基础/07-函数嵌套调用应用-函数计算.py
@@ -1,9 +1,23 @@
 # 1.求和函数
 def sum_num(a, b, c):
+    """
+    求和函数
+    :param a: 参数a
+    :param b: 参数b
+    :param c: 参数c
+    :return:  求和结果
+    """
     return a + b + c
 
 # 2.求平均值
 def average_num(a, b, c):
+    """
+    求平均值函数
+    :param a: 参数a
+    :param b: 参数b
+    :param c: 参数c
+    :return:  平均值结果
+    """
     sumResult = sum_num(a, b, c)
     return sumResult / 3
 
